c19b969216580f09d485587419d48b9ac42ba058
Ricardo-Meraz <rmeraz08022005@gmail.com>
2025-03-16 16:06:58 -0600

    pol

--- Models/ModelPoliticas.js
+++ Models/ModelPoliticas.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const PoliticasSchema = new mongoose.Schema({
-  politicaDeUso: { type: [String], required: true },
-  politicaDePrivacidad: { type: [String], required: true },
-  terminosYCondiciones: { type: [String], required: true }
+  politicaDeUso: { type: String, required: true },
+  politicaDePrivacidad: { type: String, required: true },
+  terminosYCondiciones: { type: String, required: true }
 }, { timestamps: true });
 
 const Politicas = mongoose.model("Politicas", PoliticasSchema);
