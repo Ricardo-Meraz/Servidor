0f92edfb22e42b39d8b597b04038bd73f0d8f0ff
Ricardo-Meraz <rmeraz08022005@gmail.com>
2025-03-20 02:42:26 -0600

    cambio

--- rutas/RuthDispositivos.js
+++ rutas/RuthDispositivos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Dispositivo = require('../models/Dispositivo');
+const Dispositivo = require('../Models/ModelDispositivo');
 
 // GET /dispositivos/estado?email=...
 // Devuelve el dispositivo asociado al email dado
